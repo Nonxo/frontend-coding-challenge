ec7e9dcc6217ab8d1782faf6a658aff6986e23c9
Nonso Nwabuokei <nnwabuokei@gmail.com>
2021-06-22 14:46:09 +0100

    Refactor file structure

--- portal/src/redux/actions/actionCreator.js
+++
@@ -1,3 +0,0 @@
-export const handleRequest = (type, payload = null) => ({ type, payload });
-export const handleSuccess = (type, data = null) => ({ type, data });
-export const handleError = (type, error = null) => ({ type, error });
--- portal/src/saga/index.js
+++
@@ -1,9 +0,0 @@
-import { all } from "@redux-saga/core/effects";
-import watchAbsentees from "./absentees.saga";
-import watchMembers from "./membersSaga";
-
-function* rootSaga() {
-  yield all([watchAbsentees(), watchMembers()]);
-}
-
-export default rootSaga;
